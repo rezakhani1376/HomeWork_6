76df4366caf596838b9eedcad95adb51ace7279c
reza <reza.khani1998@gmail.com>
2019-04-22 23:31:00 +0430

    add Class DemoTurners

---
+++ Turner/DemoTurners.java
@@ -0,0 +1,12 @@
+package Practice1.HomeWork_6.Turner;
+
+public class DemoTurners {
+    public static void main(String[] args) {
+        Pancake pancake=new Pancake();
+        Leaf leaf=new Leaf();
+        Page page=new Page();
+        pancake.turn();
+        leaf.turn();
+        page.turn();
+    }
+}
