3946d6e1589255999fee07beedbe81df3255eb93
reza <reza.khani1998@gmail.com>
2019-04-22 23:20:04 +0430

    impelemnt turn

---
+++ Turner/Leaf.java
@@ -0,0 +1,8 @@
+package Practice1.HomeWork_6.Turner;
+
+public class Leaf implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Changing colors");
+            }
+}
