f10212d23f049432d3927ea463437ff3010355f0
reza <reza.khani1998@gmail.com>
2019-04-22 23:23:24 +0430

    add class page

---
+++ Turner/Page.java
@@ -0,0 +1,8 @@
+package Practice1.HomeWork_6.Turner;
+
+public class Page implements Turner {
+    @Override
+    public void turn() {
+        System.out.println("Going to the next page");
+    }
+}
